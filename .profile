95e9afd6f6a9a1f9c98e6a8d9579a76b6974cafd
Julie Shapiro <julie@weeb.site>
2023-02-13 13:21:47 -0500

    add gnome color filtering and brightness output control to the button, make it a toggle

--- .profile
+++ .profile
@@ -73,4 +73,6 @@ export NO_AT_BRIDGE=1
 # enable firefox on wayland
 if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     export MOZ_ENABLE_WAYLAND=1
+    export QT_QPA_PLATFORM=wayland
+    export GDK_BACKEND=wayland
 fi
--- bin/min_backlight.sh
+++ bin/min_backlight.sh
@@ -1,5 +1,3 @@
-# backlight
-brightnessctl -d intel_backlight s 1
 # power button
 brightnessctl -d tpacpi::power s 0
 # keeb backlight
@@ -8,3 +6,24 @@ brightnessctl -d tpacpi::kbd_backlight s 0
 brightnessctl -d platform::mute s 0
 # keeb mic mute
 brightnessctl -d platform::micmute s 0
+
+#!/bin/bash
+
+key="org.gnome.desktop.a11y.applications screen-magnifier-enabled"
+current=$(gsettings get $key)
+
+if [ "$current" == "true" ]; then
+    gsettings set $key false
+else
+    # filter values
+    gsettings set org.gnome.desktop.a11y.magnifier brightness-red -0.55
+    gsettings set org.gnome.desktop.a11y.magnifier brightness-green -0.55
+    gsettings set org.gnome.desktop.a11y.magnifier brightness-blue -0.55
+    gsettings set org.gnome.desktop.a11y.magnifier color-saturation 0.10
+    # disable zoom
+    gsettings set org.gnome.desktop.a11y.magnifier mag-factor 1.0
+    # color filter
+    gsettings set $key true
+    # backlight
+    brightnessctl -d intel_backlight s 1
+fi
